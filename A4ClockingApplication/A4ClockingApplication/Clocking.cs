4bf70f03f8a81e796b61f83edc53a4f89f4114fd
Cmath06 <38296350+Cmath06@users.noreply.github.com>
2020-07-15 17:30:41 -0400

    Add files via upload
    Time Clock WindowFrom APP

---
+++ A4ClockingApplication/A4ClockingApplication/Clocking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4ClockingApp
+{
+    class Clocking
+    {
+        DateTime startTime, endTime;
+        int totalHours, todayHours;
+
+        public void clockin()
+        {
+            // get current time
+
+            //set startTime
+
+            // check exceptions
+        }
+        public void clockout()
+        {
+            //get current time
+
+            //set endTime
+
+            //check exceptions
+        }
+    }
+}
---
+++ A4ClockingApplication/A4ClockingApplication/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A4ClockingApplication
+{
+    public partial class Form1 : Form
+    {
+        Clocking mycloking;
+        public Form1()
+        {
+            InitializeComponent();
+
+            mycloking = new Clocking();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mycloking.clockin();
+            }
+            catch(ClockingTimeException cte)
+            {
+
+            }
+            catch(ClokcingHourException che)
+            {
+
+            }
+            catch (ClockingException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
