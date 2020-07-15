0959f193e057c58831b391af048cc326fd8792fe
Cmath06 <38296350+Cmath06@users.noreply.github.com>
2020-07-15 17:34:38 -0400

    Add files via upload
    C# AbstractDataPractice

---
+++ AbstractDataTypes/AbstractDataTypes/IList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AbstractDataTypes
+{
+    interface IList<T>
+    {
+        // Insert() method places one item in the list
+        void Insert(T item);
+        // Remove() method removes first instance of item in list
+        void Remove(T item);
+        // Print() method prints all items in list
+        void Print();
+    }
+}
---
+++ AbstractDataTypes/AbstractDataTypes/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AbstractDataTypes
+{
+    abstract class LinkedList<T> : IList<T> where T : IComparable
+    {
+        protected Node head, tail;
+
+        public abstract void Insert(T i);
+        public abstract void Remove(T i);
+
+        public void Print()
+        {
+            Node p = head;
+            while (p != null)
+            {
+                Console.WriteLine(p.data);
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+
+        public class Node
+        {
+            public Node next;      //Pointer
+            public T data;       //Data
+
+            public Node(T data)
+            {
+                this.data = data;
+                next = null;        //Initialized as null
+            }
+        }
+
+        protected static void Clear()
+        {
+            Clear();
+        }
+    }
+}
